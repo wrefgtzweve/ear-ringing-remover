4db10632405cf2c1241af67ca9f1681d617e3a96
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2021-12-06 16:15:34 +0100

    first commit

---
+++ lua/autorun/server/ear-ringing-remover.lua
@@ -0,0 +1,11 @@
+-- Convar
+local disableTinnitus = CreateConVar( "earringing_disable", 1, { FCVAR_ARCHIVE }, "Should the ear ringing tinnitus effect be disabled?.", 0 ):GetBool()
+cvars.AddChangeCallback( "earringing_disable", function( _, _, val )
+    disableTinnitus = tobool( val )
+end)
+
+-- Remove tinnitus
+hook.Add( "OnDamagedByExplosion", "DisableSound", function()
+    if not disableTinnitus then return end
+    return true
+end)
